6033edce6a522ceb695e1f99c8e8474f80c8a838
mayafourteen <mayafourteen.art@gmail.com>
2026-07-10 17:40:11 -0400

    Create V2 homepage prototype

---
+++ src/data/connectLinksV2.js
@@ -0,0 +1,8 @@
+export const connectLinksV2 = [
+  { href: 'https://www.youtube.com/@MayaFourteen', title: 'YouTube', type: 'fa', name: 'youtube' },
+  { href: 'https://soundcloud.com/mayafourteen', title: 'SoundCloud', type: 'fa', name: 'soundcloud' },
+  { href: 'https://www.beatport.com/pt/artist/maya-fourteen/1218920', title: 'Beatport', type: 'svg', name: 'beatport' },
+  { href: 'https://open.spotify.com/artist/6vCjW095i7Y8D7P2Uf6H0K', title: 'Spotify', type: 'fa', name: 'spotify' },
+  { href: 'https://www.instagram.com/mayafourteen/', title: 'Instagram', type: 'fa', name: 'instagram' },
+  { href: 'https://maya-fourteen-m50qvvw.gamma.site/', title: 'EPK', type: 'svg', name: 'epk' },
+];
